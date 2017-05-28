4e9cf1a3c6e2ed2affbeef191d32b7a4d67b093f
kryptn <kryptn@gmail.com>
2017-05-28 10:50:49 -0700

    Adding cli command and more setup details

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup
 
-setup(name='unserve',
-      packages=['unserve'],
-      include_package_data=True,
-      test_suite='unserve.tests',
-      install_requires=['sanic',
-                        'pyyaml',
-                        'click',])
+version = '0.1.0'
+
+classifiers = """
+Development Status :: 3 - Alpha
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python :: 3.6
+""".strip().splitlines()
+
+setup(
+    name='unserve',
+    version=version,
+    classifiers=classifiers,
+    packages=['unserve'],
+    include_package_data=True,
+    test_suite='unserve.tests',
+    entry_points={
+        'console_scripts': ['unserve = unserve.__main__:handle']
+    },
+    install_requires=[
+        'sanic',
+        'pyyaml',
+        'click',
+    ],
+)
 
